792cd9e0be938ec653b56fd41c89738e81d03277
ynjacobs <ytj64611@gamil.com>
2019-06-26 12:59:10 -0400

    function works when tested

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function removeOddSum() {
+function removeOddSum(arr){
+    let newArr = arr.filter(({a , b}) => { return (a + b) % 2 === 0;});
+
+    
+    return newArr;
 }
 
 module.exports = removeOddSum;
